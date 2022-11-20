e540f6394ef5462c7acfd716e213e8be24016b5e
Jaime Daza <jaimedaviddazat2001@gmail.com>
2022-11-19 20:59:51 -0500

    Implementación de Font Awesome para íconos

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
 import Toolbar from "./components/ui/Toolbar";
 import "./assets/styles/custom.scss";
 import Login from "./components/form/Login";
-// import logo from "./logo.svg";
-// import "./App.css";
 
 function App() {
+  library.add(far, fas);
+
   return (
     <div>
       <Toolbar />
